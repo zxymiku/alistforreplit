c821bdb3c6ac873bcae540bd8666a88df976f75d
zxymiku <95949338+zxymiku@users.noreply.github.com>
2024-01-01 03:17:21 +0000

    hender

--- web/src/pages/home/header/Header.tsx
+++ web/src/pages/home/header/Header.tsx
@@ -5,15 +5,18 @@ import {
   Center,
   Icon,
   Kbd,
+  IconButton,
 } from "@hope-ui/solid"
-import { Show } from "solid-js"
-import { getSetting, objStore, State } from "~/store"
+import { Show, Switch, Match } from "solid-js"
+import { getMainColor, getSetting, layout, objStore, State } from "~/store"
 import { BsSearch } from "solid-icons/bs"
 import { CenterLoading } from "~/components"
 import { Container } from "../Container"
 import { bus } from "~/utils"
 import { Layout } from "./layout"
-import { isMac } from "~/utils/compatibility"
+import { AiOutlineFileSearch } from "solid-icons/ai"
+import { TbListSearch } from "solid-icons/tb"
+import { CgImage } from "solid-icons/cg"
 
 export const Header = () => {
   const logos = getSetting("logo").split("\n")
@@ -41,29 +44,63 @@ export const Header = () => {
           </HStack>
           <HStack class="header-right" spacing="$2">
             <Show when={objStore.state === State.Folder}>
-              <Show when={getSetting("search_index") !== "none"}>
-                <HStack
-                  bg="$neutral4"
-                  w="$32"
-                  p="$2"
-                  rounded="$md"
-                  justifyContent="space-between"
-                  border="2px solid transparent"
-                  cursor="pointer"
-                  _hover={{
-                    borderColor: "$info6",
-                  }}
-                  onClick={() => {
-                    bus.emit("tool", "search")
-                  }}
-                >
-                  <Icon as={BsSearch} />
-                  <HStack>
-                    {isMac ? <Kbd>Cmd</Kbd> : <Kbd>Ctrl</Kbd>}
-                    <Kbd>K</Kbd>
-                  </HStack>
+              {/* <HStack
+                aria-label="Search"
+                // 背景色
+                bg="$neutral4"
+                // 长度
+                // w="$32"
+                // 方框
+                p="$2"
+                // 圆角
+                rounded="$md"
+                color={getMainColor()}
+                justifyContent="space-between"
+                border="2px solid transparent"
+                cursor="pointer"
+                _hover={{
+                  borderColor: "$info6",
+                }}
+                onClick={() => {
+                  bus.emit("tool", "search")
+                }}
+              >
+                <Icon as={AiOutlineFileSearch} />
+                <HStack>
+                  <Kbd>Ctrl</Kbd>
+                  <Kbd>K</Kbd>
                 </HStack>
-              </Show>
+              </HStack> */}
+              <IconButton
+                aria-label="Search"
+                compact
+                size="lg"
+                // 自带的搜索原本只能显示两个图标下面的换成了三个
+                // icon={
+                //   <Show when={layout() === "list"} fallback={<TbListSearch />}>
+                //     <AiOutlineFileSearch /> 
+                //   </Show>
+                // }
+                
+                //这次支持三个不同的图标了
+                icon={
+                  <Switch>
+                    <Match when={layout() === "list"}>
+                      <TbListSearch />
+                    </Match>
+                    <Match when={layout() === "grid"}>
+                      <AiOutlineFileSearch />
+                    </Match>
+                    <Match when={layout() === "image"}>
+                      <CgImage />
+                    </Match>
+                  </Switch>
+                }
+                onClick={() => {
+                  bus.emit("tool", "search")
+                }}
+              />
+              {/* 搜索右侧的那个变换的按钮 */}
               <Layout />
             </Show>
           </HStack>
